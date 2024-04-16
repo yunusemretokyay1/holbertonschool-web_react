43bc6db9090673cbb41e31b58c83cc68030af44a
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-04-16 15:21:25 +0200

    Extending the Teacher class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -14,3 +14,17 @@ interface Teacher {
     contract: false,
   };
   console.log(teacher3);
+
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+  const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
+
+  
