9d4e402e08b8b50fc47bd4aa364d58af2ea60654
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-04-17 15:55:17 +0200

    Dev servers, modules, and tree shaking

---
+++ Webpack/task_3/modules/header/header.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import '../../assets/holberton-logo.jpg';
+import './header.css';
+
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+
+console.log('Init header');
