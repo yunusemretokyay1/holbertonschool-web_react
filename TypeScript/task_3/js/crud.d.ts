4ab920fcd2f6302a8f0ad06c57faa84af86e980d
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-04-17 02:02:32 +0200

    Ambient Namespaces

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,6 @@
+import { RowID, RowElement } from './interface';
+
+export declare function insertRow(row: RowElement): number;
+export declare function deleteRow(rowId: RowID): void;
+export declare function updateRow(rowId: RowID, row: RowElement): RowID;
+
