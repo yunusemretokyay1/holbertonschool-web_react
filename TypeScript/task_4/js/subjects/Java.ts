ed28133f91700618805e0d91af65e07df4062fb2
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-04-17 02:28:11 +0200

    Namespace & Declaration merging

---
+++ TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,20 @@
+namespace Subjects {
+    export interface Teacher {
+      experienceTeachingJava?: number;
+    }
+    namespace Subjects {
+          export class Java extends Subject {
+            getRequirements() {
+              return 'Here is the list of requirements for Java';
+            }
+        
+            getAvailableTeacher() {
+              if (this.teacher && this.teacher.experienceTeachingJava > 0) {
+                return 'Available Teacher: ' + this.teacher.firstName;
+              } else {
+                return 'No available teacher'
+              }
+            }
+          }
+        }
+    }
