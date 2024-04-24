d10053f534098ff9389e1cbf2dea868daa32522d
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-04-24 12:07:53 +0200

    Modify the App

---
+++ react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+import holberton_logo from "./holberton_logo.jpg";
+import { getFullYear, getFooterCopy } from "./utils";
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={holberton_logo} />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className="login">
+        <label htmlFor="email">Email:</label>
+          <input id="email" name="email" type="email"/>
+          <label htmlFor="password">Password:</label>
+          <input id="password" name="password" type="text"/>
+          <button>OK</button>
+        </div>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
