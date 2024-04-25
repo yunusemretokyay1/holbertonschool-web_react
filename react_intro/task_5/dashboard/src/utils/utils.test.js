db979befdab25285b98934c37f5d826ffed885c7
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-04-25 04:02:57 +0200

    Testing

--- react_intro/task_5/dashboard/src/utils/utils.test.js
+++ react_intro/task_5/dashboard/src/utils/utils.test.js
@@ -1,19 +1,35 @@
-import { getLatestNotification, getFullYear, getFooterCopy } from './utils'
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
 
-describe('Test Utils.js', () => {
-  it('Function getFullYear', (done) => {
-    expect(getFullYear()).toBe(new Date().getFullYear());
-    done();
+describe('getFullYear', () => {
+  it('returns the current year', () => {
+    const fakeYear = 2024;
+    const fakeNow = new Date();
+    fakeNow.setFullYear(fakeYear);
+
+    jest.useFakeTimers({});
+    jest.setSystemTime(fakeNow);
+
+    // ASSUMES THAT `fakeNow.setFullYear`
+    // AND `setSystemTime(fakeNow)`
+    // WORK CORRECTLY.
+
+    // console.log(fakeYear, fakeNow.getFullYear());
+
+    expect(getFullYear()).toBe(fakeYear);
   });
+});
 
-  it('Function getFooterCopy', (done) => {
-    expect(getFooterCopy(true)).toBe('Holberton School');
-    expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
-    done();
+describe('getFooterCopy', () => {
+  it("returns 'Holberton School main dashboard' when called with false", () => {
+    expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
   });
+  it("returns 'Holberton School' when called with true", () => {
+    expect(getFooterCopy(true)).toEqual('Holberton School');
+  });
+});
 
-  it('Function getLatestNotification', (done) => {
-    expect(getLatestNotification()).toBe('<strong>Urgent requirement</strong> - complete by EOD');
-    done();
+describe('getLatestNotification', () => {
+  it('returns "<strong>Urgent requirement</strong> - complete by EOD"', () => {
+    expect(getLatestNotification()).toEqual('<strong>Urgent requirement</strong> - complete by EOD');
   });
 });
