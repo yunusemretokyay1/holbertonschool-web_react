bcf33df8e24c590ea77c92d1dad936eb16042e7d
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-06-05 01:04:02 +0200

    Create a small store

--- react_redux_connectors_and_providers/task_0/dashboard/src/App/App.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/App/App.js
@@ -152,4 +152,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default App;
+export { App };
--- react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { uiReducer } from './reducers/uiReducer'; // Named import
 import App from './App/App';
 
-const rootId = document.getElementById("root");
-ReactDOM.render(
+// Redux store oluşturma
+const store = configureStore({
+  reducer: {
+    ui: uiReducer,
+  },
+});
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>, rootId
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
