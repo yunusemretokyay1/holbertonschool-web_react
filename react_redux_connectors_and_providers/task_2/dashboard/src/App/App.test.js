ad4df1b739d17873ef69dba9b1162058a69f2aa1
yunusemretokyay1 <101728985+yunusemretokyay1@users.noreply.github.com>
2024-06-05 17:15:43 +0200

    App.test

--- react_redux_connectors_and_providers/task_2/dashboard/src/App/App.test.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/App/App.test.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import { App, mapStateToProps } from "./App";
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 import { fromJS } from 'immutable';
 
 describe('Tests the App component', () => {
     let wrapper;
     beforeAll(() => {
         wrapper = shallow(<App />);
-    });
-    afterEach(() => {
-        jest.restoreAllMocks();
     });
     it('Tests that App renders without crashing', () => {
         expect(wrapper.exists()).toBe(true);
@@ -18,31 +17,16 @@ describe('Tests the App component', () => {
         expect(wrapper.find('Notifications')).toHaveLength(1);
     });
     it('should contain the Header component', () => {
-        expect(wrapper.find('Header')).toHaveLength(1);
+        expect(wrapper.find(Header)).toHaveLength(1);
     });
     it('should contain the Login component', () => {
         expect(wrapper.find('Login')).toHaveLength(1);
     });
     it('should contain the Footer component', () => {
-        expect(wrapper.find('Footer')).toHaveLength(1);
+        expect(wrapper.find(Footer)).toHaveLength(1);
     });
     it('checks that CourseList is not displayed', () => {
         expect(wrapper.find('CourseList')).toHaveLength(0);
-    });
-    it('checks that the logOut function and the alert function is called with the good string', () => {
-        window.alert = jest.fn();
-        const wrapper = mount(<App />);
-        wrapper.setState({user: {isLoggedIn: true}});
-        expect(wrapper.state().user.isLoggedIn).toBe(true);
-        const event = new KeyboardEvent('keydown', { key: 'h', ctrlKey: true });
-        window.dispatchEvent(event);
-        expect(window.alert).toHaveBeenCalledWith('Logging you out');
-        expect(wrapper.state().user.isLoggedIn).toBe(false);
-        jest.restoreAllMocks();
-    });
-    it('checks that the logIn function updates the state correctly', () => {
-        wrapper.instance().logIn();
-        expect(wrapper.state().user.isLoggedIn).toBe(true);
     });
 });
 
@@ -62,24 +46,15 @@ describe('Tests the App component when isLoggedIn is true', () => {
         ];
     });
     it('Tests that the Login component is not included.', () => {
-        const wrapper = shallow(<App/>);
-        wrapper.setState({user});
+        const wrapper = shallow(<App isLoggedIn={true}/>);
         expect(wrapper.find('Login')).toHaveLength(0);
     });
     it('should contain the Notifications component', () => {
-        const wrapper = shallow(<App/>);
-        wrapper.setState({user});
+        const wrapper = shallow(<App isLoggedIn={true}/>);
         expect(wrapper.find('CourseList')).toHaveLength(1);
-    });
-    it('checks that the logOut function updates the state correctly', () => {
-        const wrapper = shallow(<App/>);
-        wrapper.setState({user});
-        wrapper.instance().logOut();
-        expect(wrapper.state().user.isLoggedIn).toBe(false);
     });
     it('checks that markNotificationAsRead works as intended', () => {
         const wrapper = shallow(<App/>);
-        wrapper.setState({user});
         expect(wrapper.state().listNotifications.length).toEqual(3);
         wrapper.instance().markNotificationAsRead(1);
         expect(wrapper.state().listNotifications.length).toEqual(2);
